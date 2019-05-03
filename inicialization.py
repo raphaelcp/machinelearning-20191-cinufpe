3ab7281ee5abb6279317e8ed1827d4d76dc868e8
raphaelcp <rcp@cin.ufpe.br>
2019-05-03 20:42:11 -0300

    update init names

--- inicialization.py
+++ inicialization.py
@@ -1,20 +1,17 @@
 import numpy as np 
 
 def init_gmedoid(k_cluster, n_views, n_exemp):
-	g_medoid = []
-	while len(g_medoid) < k_cluster*n_views:
+	g_medoid_vector = []
+	while len(g_medoid_vector) < k_cluster*n_views:
 		x = np.random.randint(1,(n_exemp+1))
-		if x not in g_medoid:
-			g_medoid.append(x)
+		if x not in g_medoid_vector:
+			g_medoid_vector.append(x)
 
-	matriz = []
+	g_medoid_matrix = []
 	for i in range(n_views):
-		matriz.append(g_medoid[(i*k_cluster):((i+1)*k_cluster)])	
+		g_medoid_matrix.append(g_medoid_vector[(i*k_cluster):((i+1)*k_cluster)])	
 
-	# g_medoid_1 = g_medoid[:10]
-	# g_medoid_2 = g_medoid[10:20]
-	# g_medoid_3 = g_medoid[20:30]
-	return matriz
+	return g_medoid_matrix
 
 
 def init_weights(k_cluster, n_views):
