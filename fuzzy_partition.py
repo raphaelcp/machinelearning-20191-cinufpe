2b370887d077fcb133b3f59ad327ab6ba12adb80
raphaelcp <rcp@cin.ufpe.br>
2019-05-14 19:21:33 -0300

    Fuzzy matrix partition function

---
+++ fuzzy_partition.py
@@ -0,0 +1,57 @@
+import numpy as np 
+
+def weighted_dist(lmbd, ex_i, ex_g, diss_matrices):
+	"""
+	"""
+	sum = 0
+	for j in range(len(diss_matrices)):
+		sum += lmbd[j] * np.linalg.norm(np.array(diss_matrices[j][ex_i]) - np.array(diss_matrices[j][ex_g[j]]))
+	return sum
+
+def fuzzy_unit(i, k, lambda_matrix, g_matrix, diss_matrices, K, m):
+	"""
+	"""
+	sum = 0
+	for h in range(K):
+		a = weighted_dist(lambda_matrix[k], i, g_matrix[k], diss_matrices)
+		b = weighted_dist(lambda_matrix[h], i, g_matrix[h], diss_matrices)
+		sum += (a/b)**(1/(m-1))
+
+	return 1/sum
+
+def fuzzy_matrix(lambda_matrix, g_matrix, diss_matrices, K, m):
+	"""
+	"""
+	U = np.zeros((len(diss_matrices), K))
+
+	for i in range(len(diss_matrices)):
+		for k in range(K):
+			U[i][k] = fuzzy_unit(i, k, lambda_matrix, g_matrix, diss_matrices, K, m)
+
+	return U
+
+if __name__ == "__main__":
+	i = 1
+	k = 0
+	lambda_matrix = [
+		[1., 1., 1.],
+		[1., 1., 1.],
+	]
+	g_matrix = [
+		[0, 1, 1], # classe 0 nas views 1, 2 e 3
+		[2, 2, 0],
+		# [0, 2], 
+		# [1, 2], 
+		# [3, 1], 
+	]
+	diss_matrices =  [ # p*n*n
+		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
+		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
+		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
+	]
+	p = 3
+
+	# print(weighted_dist(lambda_matrix[k], i, g_matrix[k], diss_matrices))
+	# print(fuzzy_partition(1, 0, lambda_matrix, g_matrix, diss_matrices, 2, 1.6)+
+	# 	fuzzy_partition(1, 1, lambda_matrix, g_matrix, diss_matrices, 2, 1.6))
+	print(fuzzy_matrix(lambda_matrix, g_matrix, diss_matrices, 2, 1.6))
