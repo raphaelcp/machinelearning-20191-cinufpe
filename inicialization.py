8756950a1a57d0b0a14604fdb606275fbe6bbddf
raphaelcp <rcp@cin.ufpe.br>
2019-05-14 21:31:03 -0300

    retorna matrix de inicializacoes transpostas e fixa o seed do rand

--- inicialization.py
+++ inicialization.py
@@ -1,6 +1,13 @@
 import numpy as np 
 
 def init_gmedoid(k_cluster, n_views, n_exemp):
+	# g_medoid_vector = [[]]*k_cluster
+	# for i in range(k_cluster):
+	# 	arr = np.array(range(n_views))
+	# 	np.random.shuffle(arr)
+	# 	g_medoid_vector[i] = arr
+
+	# return np.array(g_medoid_vector)
 
 	g_medoid_vector = []
 	while len(g_medoid_vector) < k_cluster*n_views:
@@ -12,15 +19,16 @@ def init_gmedoid(k_cluster, n_views, n_exemp):
 	for i in range(n_views):
 		g_medoid_matrix.append(g_medoid_vector[(i*k_cluster):((i+1)*k_cluster)])	
 
-	return g_medoid_matrix
+	return np.array(g_medoid_matrix).T
 
 
 def init_weights(k_cluster, n_views):
 	
-	return [[1.]*k_cluster]*n_views
+	return np.ones((k_cluster, n_views))
 
 
 if __name__=="__main__":
-	print(init_gmedoid(10, 3, 2000))
-	print(init_weights(10,3))
+	np.random.seed(42)
+	print(init_gmedoid(2, 3, 9))
+	print(init_weights(2, 3))
 	
