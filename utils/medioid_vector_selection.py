fe2592f904483fda9460d7d6760a0c1d4e0236f2
denisson leal <denissonleal@gmail.com>
2019-05-17 12:05:09 -0300

    calculo da nova matriz G

--- utils/medioid_vector_selection.py
+++ utils/medioid_vector_selection.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def arg_min(diss_matrix, U, k, m):
+	"""
+	eq 4
+	"""
 	n = len(diss_matrix)
 	amin = np.inf
+	ind_l = -1
 
 	for h in range(n):
 		dsum = 0
@@ -11,11 +15,21 @@ def arg_min(diss_matrix, U, k, m):
 
 		if amin > dsum:
 			amin = dsum
+			ind_l = h
 
-	return amin
+	return ind_l
 
-# def medioid_vector_selection(diss_matrices):
-		
+def medioid_vector_selection(diss_matrices, U, m):
+	p = len(diss_matrices)
+	K = len(U[0])
+	new_G = np.zeros((K, p), dtype=int)
+
+	for k in range(K):
+		for j in range(p):
+			new_G[k][j] = arg_min(diss_matrices[k], U, k, m)
+
+
+	return new_G
 
 
 if __name__ == '__main__':
@@ -37,5 +51,6 @@ if __name__ == '__main__':
 	k = 1
 	m = 1.6
 
-	print(diss_matrices)
-	print(arg_min(diss_matrices[0], U, k, m))
+	# print(diss_matrices[0])
+	# print(arg_min(diss_matrices[0], U, k, m))
+	print(medioid_vector_selection(diss_matrices, U, m))
