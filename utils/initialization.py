f5ea19ee5041456557faf6a939312ca91bc6cc2d
raphaelcp <rcp@cin.ufpe.br>
2019-05-18 16:17:35 -0300

    updates init_g func

--- utils/initialization.py
+++ utils/initialization.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def init_gmedoid(k_cluster, n_views, n_exemp):
-	g_medoid_matrix = [[]]*n_views
-	for i in range(n_views):
+def init_gmedoid(K, p, n):
+	g_medoid_matrix = [[]]*p
+	for i in range(p):
 		g_medoid_vector = []
-		while len(g_medoid_vector) < k_cluster:
-			x = np.random.randint(1,(n_exemp+1))
+		while len(g_medoid_vector) < K:
+			x = np.random.randint(0, n)
 			if x not in g_medoid_vector:
 				g_medoid_vector.append(x)
 
@@ -14,20 +14,20 @@ def init_gmedoid(k_cluster, n_views, n_exemp):
 	return np.array(g_medoid_matrix).T
 
 	# g_medoid_vector = []
-	# while len(g_medoid_vector) < k_cluster*n_views:
-	# 	x = np.random.randint(1,(n_exemp+1))
+	# while len(g_medoid_vector) < K*p:
+	# 	x = np.random.randint(1,(n+1))
 	# 	if x not in g_medoid_vector:
 	# 		g_medoid_vector.append(x)
 
 	# g_medoid_matrix = []
-	# for i in range(n_views):
-	# 	g_medoid_matrix.append(g_medoid_vector[(i*k_cluster):((i+1)*k_cluster)])
+	# for i in range(p):
+	# 	g_medoid_matrix.append(g_medoid_vector[(i*K):((i+1)*K)])
 
 	# return np.array(g_medoid_matrix).T
 
 
-def init_weights(k_cluster, n_views):
-	return np.ones((k_cluster, n_views))
+def init_weights(K, p):
+	return np.ones((K, p))
 
 if __name__=="__main__":
 	np.random.seed(42)
