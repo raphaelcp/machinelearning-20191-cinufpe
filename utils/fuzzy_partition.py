af2801755ce57b693137095f9b8c31cab319baed
denisson leal <denissonleal@gmail.com>
2019-05-18 16:19:03 -0300

    variaveis do fuzzy_partition segundo o dicionario

--- utils/fuzzy_partition.py
+++ utils/fuzzy_partition.py
@@ -1,64 +1,64 @@
 import numpy as np
 
-def weighted_dist(lmbd, ex_i, ex_g, diss_matrices):
+def weighted_dist(lmbd, ex_i, ex_g, D_matrices):
 	"""
 	sum de j=1 a p lambda[k][j]*d[j](e[i], g[k][j])
 
 	lmbd -> matriz lambda
 	ex_i -> exemplo i para calc das distancias
 	ex_g -> vetor com a referencia para os representantes
-	diss_matrices -> matriz das distancias
+	D_matrices -> matriz das distancias
 	"""
 	wsum = 0
 	for j in range(len(ex_g)):
-		wsum += lmbd[j] * diss_matrices[j][ex_i][ex_g[j]]
+		wsum += lmbd[j] * D_matrices[j][ex_i][ex_g[j]]
 	return wsum
 
-def fuzzy_unit(i, k, lambda_matrix, g_matrix, diss_matrices, K, m):
+def fuzzy_unit(i, k, W, G, D_matrices, K, m):
 	"""
 	"""
 	fsum = 0
 	for h in range(K):
-		a = weighted_dist(lambda_matrix[k], i, g_matrix[k], diss_matrices)
-		b = weighted_dist(lambda_matrix[h], i, g_matrix[h], diss_matrices)
+		a = weighted_dist(W[k], i, G[k], D_matrices)
+		b = weighted_dist(W[h], i, G[h], D_matrices)
 		fsum += (a/b)**(1/(m-1))
 
 	return 1/fsum
 
-def fuzzy_matrix(lambda_matrix, g_matrix, diss_matrices, K, m):
+def fuzzy_matrix(W, G, D_matrices, K, m):
 	"""
 	calculo da eq 6
 	"""
-	U = np.zeros((len(diss_matrices), K))
+	U = np.zeros((len(D_matrices), K))
 
-	for i in range(len(diss_matrices)):
+	for i in range(len(D_matrices)):
 		for k in range(K):
-			U[i][k] = fuzzy_unit(i, k, lambda_matrix, g_matrix, diss_matrices, K, m)
+			U[i][k] = fuzzy_unit(i, k, W, G, D_matrices, K, m)
 
 	return U
 
 if __name__ == "__main__":
 	i = 1
 	k = 0
-	lambda_matrix = [
+	W = [
 		[1., 1., 1.],
 		[1., 1., 1.],
 	]
-	g_matrix = [
+	G = [
 		[0, 1, 1], # classe 0 nas views 1, 2 e 3
 		[2, 2, 0],
 		# [0, 2],
 		# [1, 2],
 		# [3, 1],
 	]
-	diss_matrices =  [ # p*n*n
+	D_matrices =  [ # p*n*n
 		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
 		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
 		[[1., 2., 3.],[4., 5., 6.],[7., 8., 9.]],
 	]
 	p = 3
 
-	# print(weighted_dist(lambda_matrix[k], i, g_matrix[k], diss_matrices))
-	# print(fuzzy_partition(1, 0, lambda_matrix, g_matrix, diss_matrices, 2, 1.6)+
-	# 	fuzzy_partition(1, 1, lambda_matrix, g_matrix, diss_matrices, 2, 1.6))
-	print(fuzzy_matrix(lambda_matrix, g_matrix, diss_matrices, 2, 1.6))
+	# print(weighted_dist(W[k], i, G[k], D_matrices))
+	# print(fuzzy_partition(1, 0, W, G, D_matrices, 2, 1.6)+
+	# 	fuzzy_partition(1, 1, W, G, D_matrices, 2, 1.6))
+	print(fuzzy_matrix(W, G, D_matrices, 2, 1.6))
