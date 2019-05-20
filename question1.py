c8cf3594fec280feeb3aaf0b01c99be6291c57d8
denisson leal <denissonleal@gmail.com>
2019-05-20 16:56:25 -0300

    coloca para rodar 100x o algoritmo e pega o melhor resultado

--- question1.py
+++ question1.py
@@ -5,7 +5,8 @@ from utils.fuzzy_partition import fuzzy_matrix
 from utils.adequacy_function import adequacy
 from utils.medioid_vector_selection import medioid_vector_selection
 from utils.vectors_relevance_weights import vectors_relevance_weights
-from utils.vectors_relevance_weights import vectors_relevance_weights
+from utils.convert_crisp import convert_crisp
+import threading
 
 def MVFCMddV(D_matrices, K=10, m=1.6, eps=10**(-10), T=150):
 	t = 0
@@ -25,22 +26,60 @@ def MVFCMddV(D_matrices, K=10, m=1.6, eps=10**(-10), T=150):
 		print('time %d'%(t))
 
 		G = medioid_vector_selection(D_matrices, U, m)
+		# np.savetxt('output/G'+str(t), G, fmt='%i')
+		# print('calc G')
 
 		W = vectors_relevance_weights(D_matrices, G, U, m)
+		# np.savetxt('output/W'+str(t), W, fmt='%.7f')
+		# print('calc W')
 
 		U = fuzzy_matrix(W, G, D_matrices, K, m)
+		# np.savetxt('output/U'+str(t), U, fmt='%.7f')
+		# print('calc U')
 
 		u.append(adequacy([G, W, U], m, D_matrices))
 
 		if abs(u[t]-u[t-1]) < eps or t >= T:
 			break
 
+	return {
+		'performance': u[t],
+		'G': G,
+		'W': W,
+		'U': U,
+	}
+	# crisp = convert_crisp(U)
+	# np.savetxt('output/crisp', crisp, fmt='%d')
+
+	# qtdpclass = len(crisp)/K
+	# resume_matrix = np.zeros((K, K))
+	# for i in range(K):
+	# 	subset = crisp[int(i*qtdpclass):int((i+1)*qtdpclass)]
+	# 	resume_matrix[i] = np.sum(subset, axis=0)
+
+	# np.savetxt('output/resume', resume_matrix, fmt='%d')
+
+
+def save_result(obj, it):
+	np.savetxt('output/%s-G'%(it), obj['G'], fmt='%i')
+	np.savetxt('output/%s-W'%(it), obj['W'], fmt='%.7f')
+	np.savetxt('output/%s-U'%(it), obj['U'], fmt='%.7f')
+	np.savetxt('output/%s-performance'%(it), [obj['performance']], fmt='%.7f')
+
 
 def main():
 	D_matrices = normalized_dissimilarity(
-		list_files=('mfeat-fac', 'mfeat-fou', 'mfeat-kar')
+		list_files=('mfeat-fac-test', 'mfeat-fou-test', 'mfeat-kar-test')
 	)
-	MVFCMddV(D_matrices)
+
+	best_out = {}
+	for i in range(100):
+		print('run=%d'%(i))
+		out = MVFCMddV(D_matrices)
+		save_result(out, '%02d'%(i))
+		if i == 0 or out['performance'] < best_out['performance']:
+			best_out = out.copy()
+			save_result(best_out, 'best')
 
 
 if __name__ == '__main__':
