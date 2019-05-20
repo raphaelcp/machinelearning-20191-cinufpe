a67cf5e53d68a86f33bd25ffc4a35e5a8799e0bd
denisson leal <denissonleal@gmail.com>
2019-05-20 16:48:21 -0300

    inicio do calculo do indice de rand

---
+++ utils/rand_index.py
@@ -0,0 +1,13 @@
+
+def rand_index(crisp_matrix, K):
+	sum_a = 0
+	sum_b = 0
+	sum_c = 0
+	sum_d = 0
+
+	qtd_by_class = int(len(crisp_matrix)/K)
+
+	for k in range(K):
+		for i in range(qtd_by_class):
+			pass
+
