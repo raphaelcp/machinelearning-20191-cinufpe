3d7d34b06479bf837185f35922ef2b3142498750
raphaelcp <rcp@cin.ufpe.br>
2019-05-20 21:09:55 -0300

    calculo de indice de rand

--- utils/rand_index.py
+++ utils/rand_index.py
@@ -1,13 +1,35 @@
+import numpy as np 
+from sklearn.metrics.cluster import adjusted_rand_score
+from utils.convert_crisp import convert_crisp
 
-def rand_index(crisp_matrix, K):
-	sum_a = 0
-	sum_b = 0
-	sum_c = 0
-	sum_d = 0
 
-	qtd_by_class = int(len(crisp_matrix)/K)
+def true_labels(n, K):
+	tl = np.zeros((n))
+	qtd_by_class = int(n/K)
+	for i in range(n):
+		# print(i, int(i/K))
+		tl[i] = int(i/qtd_by_class)
+	return tl
 
-	for k in range(K):
-		for i in range(qtd_by_class):
-			pass
 
+def adjusted_rand_index(crisp_matrix):
+	n = len(crisp_matrix)
+	new_crisp = np.zeros(n)
+	for i in range(n):
+		new_crisp[i] = np.argmax(crisp_matrix[i])
+	print(new_crisp)
+	return adjusted_rand_score(new_crisp, true_labels(crisp_matrix.shape[0], crisp_matrix.shape[1]))
+
+if __name__ == '__main__':
+	# print(true_labels(6, 3))
+
+	crisp_matrix = np.array([
+		[1,0,0], 
+		[1,0,0],
+		[1,0,0],
+		[0,1,0],
+		[0,0,1],
+		[0,1,0],
+	])
+
+	print(rand_index(crisp_matrix))
