bd6914c2e5ea45c43f57fd03f007b654f0b605b8
raphaelcp <rcp@cin.ufpe.br>
2019-05-24 15:32:31 -0300

    divisão do calculo do vetor crisp

--- utils/rand_index.py
+++ utils/rand_index.py
@@ -1,23 +1,22 @@
 import numpy as np 
-from sklearn.metrics.cluster import adjusted_rand_score
 from utils.convert_crisp import convert_crisp
 
 
 def true_labels(n, K):
-	tl = np.zeros((n))
+	tl = np.zeros(n, dtype=int)
 	qtd_by_class = int(n/K)
 	for i in range(n):
 		# print(i, int(i/K))
 		tl[i] = int(i/qtd_by_class)
 	return tl
 
-
-def adjusted_rand_index(crisp_matrix):
+def new_crisp(crisp_matrix):
 	n = len(crisp_matrix)
-	new_crisp = np.zeros(n)
+	crisp_vec = np.zeros(n, dtype=int)
 	for i in range(n):
-		new_crisp[i] = np.argmax(crisp_matrix[i])
-	return adjusted_rand_score(new_crisp, true_labels(crisp_matrix.shape[0], crisp_matrix.shape[1]))
+		crisp_vec[i] = np.argmax(crisp_matrix[i])
+	return crisp_vec
+
 
 if __name__ == '__main__':
 	# print(true_labels(6, 3))
