6bba0b57c275d8790443bb50ce277b1af808de5d
denisson leal <denissonleal@gmail.com>
2019-06-01 17:17:53 -0300

    muda saida aparente do algoritmo MVFCMddV

--- mvfcmddv/__init__.py
+++ mvfcmddv/__init__.py
@@ -7,67 +7,40 @@ from .vectors_relevance_weights import vectors_relevance_weights
 import time
 
 def MVFCMddV(D_matrices, K, m=1.6, eps=10**(-10), T=150):
+	microtime = time.time()
 	t = 0
 	p = D_matrices.shape[0]
 	n = D_matrices.shape[1]
 
-	#Inicialização dos dados
+	# Inicialização dos dados
 	G = init_gmedoid(K, p, n)
 	W = init_weights(K, p)
 	U = fuzzy_matrix(W, G, D_matrices, K, m, [])
-	# print(U)
-	# exit()
 
-	u = [];
-	u.append(adequacy([G, W, U], m, D_matrices, U**m))
-	# print(u)
+	J = [];
+	J.append(adequacy([G, W, U], m, D_matrices, U**m))
+	print("J[%d]=%.9f - %.3fs"%(t, J[t], time.time()-microtime))
+
 	while True:
 		t += 1
-		print('time %d'%(t))
-
 		microtime = time.time()
+
 		Uem = U**m
-		# print('- calc Uem %.3f'%(time.time()-microtime))
 
-		# print(G)
-		microtime = time.time()
 		G = medioid_vector_selection(D_matrices, Uem, m)
-		# np.savetxt('output/G'+str(t), G, fmt='%i')
-		# print('- calc G %.3f'%(time.time()-microtime))
-		# u.append(adequacy([G, W, U], m, D_matrices, Uem))
-
-		microtime = time.time()
 		W = vectors_relevance_weights(D_matrices, G, Uem, m)
-		# np.savetxt('output/W'+str(t), W, fmt='%.7f')
-		# print('- calc W %.3f'%(time.time()-microtime))
-		# u.append(adequacy([G, W, U], m, D_matrices, Uem))
-
-		microtime = time.time()
 		U = fuzzy_matrix(W, G, D_matrices, K, m, U)
-		# np.savetxt('output/U'+str(t), U, fmt='%.7f')
-		# print('- calc U %.3f'%(time.time()-microtime))
-		# u.append(adequacy([G, W, U], m, D_matrices, Uem))
 
-		microtime = time.time()
-		u.append(adequacy([G, W, U], m, D_matrices, Uem))
-		# print('- calc u %.3f'%(time.time()-microtime))
-		# print(u)
-		if abs(u[t]-u[t-1]) < eps or t >= T:
+		J.append(adequacy([G, W, U], m, D_matrices, Uem))
+
+		print("J[%d]=%.9f - %.3fs"%(t, J[t], time.time()-microtime))
+		if abs(J[t]-J[t-1]) < eps or t >= T:
 			break
 
 	return {
-		'performance': u[t],
+		'performance': J[t],
+		'J': J,
 		'G': G,
 		'W': W,
 		'U': U,
 	}
-	# crisp = convert_crisp(U)
-	# np.savetxt('output/crisp', crisp, fmt='%d')
-
-	# qtdpclass = len(crisp)/K
-	# resume_matrix = np.zeros((K, K))
-	# for i in range(K):
-	# 	subset = crisp[int(i*qtdpclass):int((i+1)*qtdpclass)]
-	# 	resume_matrix[i] = np.sum(subset, axis=0)
-
-	# np.savetxt('output/resume', resume_matrix, fmt='%d')
